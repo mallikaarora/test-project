4efd8e11643ac402cd3d741e47cd4f6df29a74b1
mallikaarora <mallika14arora@gmail.com>
2019-12-09 15:01:23 +0530

    Updates

---
+++ precache-manifest.4c6d42792f80e9e40395c68e52a5d241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c473920f916ba8f9ddf",
+    "url": "/test-project/static/js/runtime~main.8e57902c.js"
+  },
+  {
+    "revision": "ba54149e8b5e9695c97e",
+    "url": "/test-project/static/js/main.c9e101f4.chunk.js"
+  },
+  {
+    "revision": "5ad9e9d6fec0ec135cb7",
+    "url": "/test-project/static/js/2.f6711c6e.chunk.js"
+  },
+  {
+    "revision": "ba54149e8b5e9695c97e",
+    "url": "/test-project/static/css/main.b20d18e4.chunk.css"
+  },
+  {
+    "revision": "666441fcf54df3f5d4095bb5b22320b7",
+    "url": "/test-project/index.html"
+  }
+];
